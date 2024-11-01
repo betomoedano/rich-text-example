e00873b4b93cda983b964a597a7c770646f52ae6
betomoedano <betomoedano@outlook.com>
2024-11-01 14:15:52 -0500

    Dom components are working! 🥲

--- components/dom-components/hello-dom.tsx
+++ components/dom-components/hello-dom.tsx
@@ -3,7 +3,8 @@
 export default function DOMComponent({ name }: { name: string }) {
   return (
     <div>
-      <h1>Hello, {name}</h1>
+      <h1>Hello testing one, {name}</h1>
+      <button onClick={() => alert("Hello")}>Click me</button>
     </div>
   );
 }
